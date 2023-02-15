7080d4fb6bdc20181abf3d7bd7bbf9df14ae4688
Mohammadibbu <92036859+Mohammadibbu@users.noreply.github.com>
2023-02-15 18:06:38 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,7 +10,7 @@ const searchRecord = async (value) => {
 
   const jsonData = await getJSONData();
 
-  const recordFound = jsonData.find((record) =>  record.code === value.toUpperCase());
+  const recordFound = jsonData.find((record) =>  record.code === value.toUpperCase().startsWith(record.code));
   
   // the above code changed, the code can be written even more like this.
   // check this: https://github.com/anburocky3/RTO-NumberPlate-Finder/issues/1
